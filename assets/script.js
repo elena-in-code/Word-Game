17820fbda0bd82197846d0599a0bb1d2e67e6728
elena-in-code <30567608+elena-in-code@users.noreply.github.com>
2017-08-16 18:23:34 +0200

    js finish
    added requiered field

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,21 @@
 window.onload = function(){
-  var submitButton = document.getElementById("submitButton");
-  submitButton.addEventListener("click", buttonFunc);
-
-  function buttonFunc(){
-    var animal = document.getElementById("animal").value;
- 	var verb = document.getElementById("verb").value;
- 	var adj = document.getElementById("adj").value;
- 	var adv = document.getElementById("adv").value;
-    var output = "The "+adj+" "+animal+" "+verb+" very "+adv+", in fact, thanks to that the "+adj+" "+animal+" gets all the popcorn that an animal can ever wish for. And we all love popcorn!!";
-		
-	document.getElementById("madLibsHeader").style.display = "block";
-    document.getElementById("output").innerHTML = output;
-
-  }
+	var submitButton = document.getElementById("submitButton");
+  	submitButton.addEventListener("click", buttonFunc());
+	
+	function buttonFunc(){
+		var animal = document.getElementById("animal").value;
+		var verb = document.getElementById("verb").value;
+		var adj = document.getElementById("adj").value;
+		var adv = document.getElementById("adv").value;
+		var output = "The "+adj+" "+animal+" "+verb+" very "+adv+", in fact, thanks to that the "+adj+" "+animal+" gets all the popcorn that an animal can ever wish for. And we all love popcorn!!";
+	    
+	    document.getElementById("madLibsHeader").style.display = "block";
+	    document.getElementById("output").innerHTML = output;
+  	}
 }
 
 //Reset::::
 var resetButton = document.querySelector(".play-again");
-
 //Reset function
 resetButton.addEventListener("click", function(){
 	clearFields();
@@ -34,3 +32,4 @@ function clearFields() {
 function clearoutput() {
 	document.getElementById("output").innerHTML = "";
 }
+
