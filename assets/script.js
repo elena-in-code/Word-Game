b68ea8889a65db0ba253b4e3cc05288e45bb8ef9
elena-in-code <30567608+elena-in-code@users.noreply.github.com>
2017-08-16 11:33:55 +0200

    WIP reset feature js
    working on the reset function js

--- assets/script.js
+++ assets/script.js
@@ -17,3 +17,15 @@ window.onload = function(){
   }
 }
 
+//Reset feature::WIP::
+var resetButton = document.querySelector(".play-again");
+
+function reset (){
+	//needs to clear the previous generated story
+	//needs to empty/clear all the input boxes
+	//make the page ready like it was refreshed
+}
+//Reset button
+resetButton.addEventListener("click", function(){
+	reset();
+})
