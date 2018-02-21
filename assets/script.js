8cb8054912295eaa8cb378f8fe77b4c3824ac41a
elena-in-code <30567608+elena-in-code@users.noreply.github.com>
2018-02-21 13:07:57 +0100

    fixed logic
    and change the styling

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,31 @@
-window.onload = function(){
-	var submitButton = document.getElementById("submitButton");
-  	submitButton.addEventListener("click", buttonFunc());
+var submitButton = document.getElementById("submitButton");
+var outputContainer = document.getElementById("output");
+
+submitButton.addEventListener("click", buttonFunc);
+
+function buttonFunc(e) {
+	const animalValue = document.getElementById("animal").value;
+	const verbValue = document.getElementById("verb").value;
+	const adjValue = document.getElementById("adj").value;
+	const advValue = document.getElementById("adv").value;
+	const output = `
+	The ${adjValue} ${animalValue} ${verbValue} 
+	very ${advValue}, in fact, thanks to that the 
+	${adjValue} ${animalValue} gets all the popcorn that 
+	an animal can ever wish for. And we all love popcorn 🍿!!
+	`;
+	document.getElementById("output").innerHTML = output;
+	outputContainer.classList.remove("hide");
+	e.preventDefault();
 	
-	function buttonFunc(){
-		var animal = document.getElementById("animal").value;
-		var verb = document.getElementById("verb").value;
-		var adj = document.getElementById("adj").value;
-		var adv = document.getElementById("adv").value;
-		var output = "The "+adj+" "+animal+" "+verb+" very "+adv+", in fact, thanks to that the "+adj+" "+animal+" gets all the popcorn that an animal can ever wish for. And we all love popcorn!!";
-	    
-	    document.getElementById("madLibsHeader").style.display = "block";
-	    document.getElementById("output").innerHTML = output;
-  	}
 }
+
 
 //Reset::::
 var resetButton = document.querySelector(".play-again");
 //Reset function
 resetButton.addEventListener("click", function(){
-	clearFields();
-	clearoutput();
+	window.location.reload(true);
 })
-//clear input fields
-function clearFields() {
-    document.getElementById("animal").value = "";
-	document.getElementById("verb").value = "";
-	document.getElementById("adj").value = "";
-	document.getElementById("adv").value = "";
-}
-// clear output
-function clearoutput() {
-	document.getElementById("output").innerHTML = "";
-}
+
 
